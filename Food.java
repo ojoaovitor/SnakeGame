e4280600987b7a24a1387d309d2bdbdef2690b74
João Vitor <99853352+ojoaovitor@users.noreply.github.com>
2022-07-11 16:26:33 -0300

    Add files via upload

---
+++ Food.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.awt.Rectangle;
+
+public class Food {
+	private int x;
+	private int y;
+	
+	public Food(Snak player) {
+		this.random_spawn(player);
+	}
+	
+	public void random_spawn(Snak player) {
+		boolean onSnake = true;
+		while(onSnake) {
+			onSnake = false;
+			x = (int)(Math.random() * Game.width);
+			y = (int)(Math.random() * Game.height);
+			
+			for (Rectangle r : player.getBody()) {
+				if(r.x == x && r.y == y) {
+					onSnake = true;
+				}
+			}
+		}
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+}
